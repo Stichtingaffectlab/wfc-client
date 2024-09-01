e0a219d6beeceacf89bb8225102b3479ef8b58ea
madhums <me@madhums.me>
2024-09-01 11:59:53 +0200

    add example comparison in code comment

--- timeline.rb
+++ timeline.rb
@@ -88,6 +88,7 @@ class Timeline
       t = ev["outside_at"] || ev["inside_at"] || ev["eats_at"]
       # @todo make sure this comparison considers a few more minutes in order to play the event for more than a mniute
       # or use some other mechanism (OR that it's not enqueued every time this mentod is called)
+      #   truncate_to_minute(Time.parse(t)) < 2.minutes.ago
       truncate_to_minute(Time.parse(t)) == truncate_to_minute(Time.current)
     end
 
