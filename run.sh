2753e09dd19f74b1347f23e4e0766a8b44aa31af
madhums <me@madhums.me>
2024-12-06 23:00:18 +0100

    use datestamp for logs

--- run.sh
+++ run.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# Start mpv with logs visible and running in the background
-nohup mpv --input-ipc-server=/tmp/mpvsocket --loop=inf --fullscreen ./videos/235_ruminations_inside.mp4 > tmp/mpv.log 2>&1 &
+# Generate timestamp in yyyy-mm-dd-hh format
+DATESTAMP=$(date +"%Y-%m-%d")
 
-echo "mpv started. Logs are being written to tmp/mpv.log"
+# Start mpv with logs visible and running in the background
+nohup mpv --input-ipc-server=/tmp/mpvsocket --loop=inf --fullscreen ./videos/235_ruminations_inside.mp4 > tmp/mpv-$DATESTAMP.log 2>&1 &
+
+echo "mpv started. Logs are being written to tmp/mpv-$DATESTAMP.log"
 
 # Start the Ruby script with logs visible and running in the background
-nohup ruby main.rb > tmp/wfc.log 2>&1 &
+nohup ruby main.rb > tmp/wfc-$DATESTAMP.log 2>&1 &
 
-echo "Ruby script started. Logs are being written to tmp/wfc.log"
+echo "Ruby script started. Logs are being written to tmp/wfc-$DATESTAMP.log"
 
 # Display information to the user
 echo "Both processes are running in the background."
